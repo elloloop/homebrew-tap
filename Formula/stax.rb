68b5c7daf56d2491345591d4e295b13353fccc50
Arun Saragadam <arun88m@gmail.com>
2026-05-04 00:13:29 +0100

    stax 2.0.0 SHAs

--- Formula/stax.rb
+++ Formula/stax.rb
@@ -7,22 +7,22 @@ class Stax < Formula
   on_macos do
     on_arm do
       url "https://github.com/elloloop/stax/releases/download/v#{version}/stax-darwin-arm64"
-      sha256 "d2d90bd8bcaef3a141ee0460f718f7fd0a4b6515f943c9e8689ae0235bbb0c65"
+      sha256 "19dcde184b07ccde6e46ac0efadea2ac6b0412dbf0224f61c34dbacdbf335fc9"
     end
     on_intel do
       url "https://github.com/elloloop/stax/releases/download/v#{version}/stax-darwin-amd64"
-      sha256 "381a5a052d024ee0c47f378d583e2ee7c25cb948d23a558793b6a28ad8d36c33"
+      sha256 "93dbe28ffabda8590721f627a3ab19d1d2ab87c8815365fb11e1edaf73532cda"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/elloloop/stax/releases/download/v#{version}/stax-linux-arm64"
-      sha256 "970c1c2d0923e447c73b6c725cfba320f3ce5474dbeff8f086ce8a257a104c2d"
+      sha256 "622bdb3001785c27476896d2dbbf145851ccc345129f2dee92fadcbe6e5ba111"
     end
     on_intel do
       url "https://github.com/elloloop/stax/releases/download/v#{version}/stax-linux-amd64"
-      sha256 "661b4737b5a79286c43f727802107b97daf441aa8ed7196e43f47c8262ee48ea"
+      sha256 "9f0cfbdfd918363ade0b29b2e3c3d2a1667793b30f9b14f5326413f26ee07e7d"
     end
   end
 
